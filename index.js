0d9ea6a7d1836a2f1ed2133d7b65a921c56d3855
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-01-24 21:02:07 +0600

    element updated

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var numbers = [10, 20, 30, 40, 50]
-
+var numbers = [10, 20, 30, 40, 50, 60, 70];
+//get element value by index
 var element = numbers[3];
-console.log(element)
+// console.log(element)
+//set element value by index
+numbers[2] = 32;
+// console.log(numbers)
+
+// find index of an element
+var positionIndex = numbers.indexOf(60);
+console.log(positionIndex);
---
+++ push-pop.js
@@ -0,0 +1,4 @@
+// use push to add element to an array
+var friendsName =['asif', 'romio', 'joy', 'ananto'];
+friendsName.push("akash");
+console.log(friendsName);
