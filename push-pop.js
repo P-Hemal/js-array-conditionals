ae93e00ae32eeae0f41c5610670a29aed02fabb6
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-01-26 21:38:01 +0600

    variable updated

--- push-pop.js
+++ push-pop.js
@@ -1,4 +1,12 @@
-// use push to add element to an array
+// use push to add element to an array as the last element of the array
 var friendsName =['asif', 'romio', 'joy', 'ananto'];
 friendsName.push("akash");
+// console.log(friendsName);
+var friendsName =['asif', 'romio', 'joy', 'ananto'];
+friendsName.pop("");
+console.log(friendsName);
+// use pop to get last element
+
+var friendsName =[10, 20, 30, 40];
+friendsName.pop();
 console.log(friendsName);
