9e1592aedfb634fcb97f1c058d46a66d4520deb5
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-01-30 20:58:38 +0600

    comparison updated

---
+++ comparison.js
@@ -0,0 +1,21 @@
+//console.log(5<6);
+//console.log(5>6);
+//console.log(5==6);
+//console.log(6==6);
+//console.log(5 != 6)
+//console.log(5 != 5)
+
+//Less then or equal
+
+//console.log(5 <= 6)
+
+//greater then or equal
+
+//console.log(5 >= 10)
+
+//multiple condition
+
+//money1 > money2 && player1 > player2 && result1 > result2
+//money1 > money2 || player1 > player2 || result1 > result2
+
+
