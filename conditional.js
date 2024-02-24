4ac20eb3a0035d116ca9f7fc76bfa9ebe2b269b1
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-24 10:07:46 +0600

    multiple variable

--- conditional.js
+++ conditional.js
@@ -1,2 +1,19 @@
 var iphonePrice = 90000;
 var myBudget = 95000;
+
+
+// if (iphonePrice < myBudget){
+//     console.log("iphone kine felbo");
+// }
+
+
+// if  (iphonePrice <= myBudget){
+//     console.log('iphone kine nibo')
+// }
+
+if  (iphonePrice < myBudget){
+    console.log('iphone kine nibo')
+}
+else {
+    console.log('xiaomi kine nibo')
+}
---
+++ multiple-conditions.js
@@ -0,0 +1,10 @@
+var isGraduated = true;
+var salary = 35000;
+
+
+if(isGraduated == true && salary > 30000){
+console.log('job peye jabo')
+}
+else{
+    console.log('job pabo na')
+}
