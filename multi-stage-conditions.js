21308e6ea5559b5d6e0ff803018952e69c6eddc5
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-24 12:48:54 +0600

    multi stage

---
+++ multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var money = 55;
+var danishPrice = 45;
+var breadPrice = 35;
+var toastPrice = 20;
+
+if(danishPrice < money){
+    console.log('danish khabo');
+}
+else if(breadPrice <= money){
+    console.log('bread khabo');
+}
+else if(toastPrice < money){
+    console.log('toast khabo');
+}
+else{
+    console.log('cha khawai valo')
+}
