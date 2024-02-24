5dea1028174594a6889c553e57696c9d1776f03c
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-24 10:21:01 +0600

    conditions

--- multiple-conditions.js
+++ multiple-conditions.js
@@ -1,10 +1,19 @@
-var isGraduated = true;
+var isGraduated = false;
 var salary = 35000;
+var cars = 0;
 
 
-if(isGraduated == true && salary > 30000){
-console.log('job peye jabo')
+// if(isGraduated == true && salary > 30000 && cars >= 1){
+// console.log('job peye jabo')
+// }
+// else{
+//     console.log('job pabo na')
+// }
+
+
+if(isGraduated == true || salary >= 30000 || cars > 1){
+    console.log('life success')
 }
 else{
-    console.log('job pabo na')
+    console.log('life hell')
 }
