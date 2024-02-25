3802d517e7bb8c13871a4a27612d779b726646d9
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-25 10:22:22 +0600

    array updated

---
+++ nested-conditions.js
@@ -0,0 +1,11 @@
+var math = true;
+var english = true;
+
+if(math == true){
+    if(english == true){
+
+    }
+    else{
+        console.log('IELTS clear korte parbo na')
+    }
+}
--- push-pop.js
+++ push-pop.js
@@ -1,8 +1,8 @@
 // use push to add element to an array as the last element of the array
-var friendsName =['asif', 'romio', 'joy', 'ananto'];
+var friendsName =['asif', 'romio', 'joy', 'Prince'];
 friendsName.push("akash");
-// console.log(friendsName);
-var friendsName =['asif', 'romio', 'joy', 'ananto', 'Emon', 'akash'];
+console.log(friendsName);
+var friendsName =['asif', 'romio', 'joy', 'Prince', 'Emon', 'akash'];
 friendsName.pop("");
 console.log(friendsName);
 // use pop to get last element
